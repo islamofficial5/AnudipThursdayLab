ba3cd531d1fa1d2e26c083b050b0c7c5ae8710b0
islamofficial5 <95912179+islamofficial5@users.noreply.github.com>
2022-09-02 14:28:15 +0530

    Add files via upload

---
+++ THursdayLab05/04-08-22/MergeArray.java
@@ -0,0 +1,44 @@
+package arrayprograms;
+import java.util.Scanner;
+
+public class MergeArray 
+{
+	public static void main(String[] args)
+	{
+		int a[]=new int[5];
+		int b[]=new int[5];
+		int c[]=new int[a.length+b.length];
+		
+		Scanner sc = new Scanner(System.in);
+		int i,p=5;
+		System.out.println("Enter 5 elements in the first array");
+		for(i=0;i<5;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		
+		System.out.println("Enter 5 elements in the the second array");
+		for(i=0;i<5;i++)
+		{
+			b[i]=sc.nextInt();
+		}
+		
+		//assigning value from first array
+		for(i=0;i<5;i++)
+		{
+			c[i]=a[i];
+		}
+		
+		//assigning value from second array
+		for(i=0;i<5;i++)
+		{
+			c[p++]=b[i];
+		}
+		//after merged, the array elements are;
+		System.out.println("After merged the array elements are:");
+		for(i=0;i<c.length;i++)
+		{
+			System.out.print(c[i]+" ");
+		}
+	}
+}
